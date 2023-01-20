c5927958d96fc8abb0356713df18f6e9b7761470
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-01-19 17:57:56 -0800

    made the prompts for eng/intern. I also made the employe js file to use as a key to extend man/eng/int to, to avoid reduncancy

--- Assets/index.js
+++ Assets/index.js
@@ -8,7 +8,7 @@ const Intern = require("./lib/intern")
 
 // inq prompts for manager
 const addManager = () => {
-    return inquirer.createPromptModule([
+    inquirer.prompt([
         {
             type: 'input',
             message: "What is the team manager's name?",
@@ -32,6 +32,64 @@ const addManager = () => {
             message: "What is the manager's office number",
             name: "manager office number",
             validate: (managerONumberInput) => { if (managerONumberInput) { return true } else { return 'Manager office number is needed.' } },
+        },
+    ])
+}
+
+const addEngineer = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is the engineer's name?",
+            name: "engineer name",
+            validate: (engineerNameInput) => { if (engineerNameInput) { return true } else { return 'Engineer name is needed.' } },
+        },
+        {
+            type: 'input',
+            message: "What is the Engineer's employee ID?",
+            name: "engineer ID",
+            validate: (engineerIdInput) => { if (engineerIdInput) { return true } else { return 'Engineer ID is needed.' } },
+        },
+        {
+            type: 'input',
+            message: "What is the Engineer's email address?",
+            name: "engineer email",
+            validate: (engineerEmailInput) => { if (engineerEmailInput) { return true } else { return 'Engineer email is needed.' } },
+        },
+        {
+            type: 'input',
+            message: "What is the Engineer's office number",
+            name: "engineer office number",
+            validate: (engineerONumberInput) => { if (engineerONumberInput) { return true } else { return 'Engineer office number is needed.' } },
+        },
+    ])
+}
+
+const addIntern = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is the intern's name?",
+            name: "intern name",
+            validate: (internNameInput) => { if (internNameInput) { return true } else { return 'Intern name is needed.' } },
+        },
+        {
+            type: 'input',
+            message: "What is the Intern's employee ID?",
+            name: "intern ID",
+            validate: (internIdInput) => { if (internIdInput) { return true } else { return 'Intern ID is needed.' } },
+        },
+        {
+            type: 'input',
+            message: "What is the Intern's email address?",
+            name: "intern email",
+            validate: (internEmailInput) => { if (internEmailInput) { return true } else { return 'Intern email is needed.' } },
+        },
+        {
+            type: 'input',
+            message: "What is the Intern's office number",
+            name: "intern office number",
+            validate: (internONumberInput) => { if (internONumberInput) { return true } else { return 'Intern office number is needed.' } },
         },
     ])
 }
